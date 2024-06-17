89b1a3c5942c55ac6cfa61331fbba59cf7096ae1
Nikita Mozgin <74683169+Mozgiii9@users.noreply.github.com>
2024-06-17 14:05:53 +0300

    Update artela.sh

--- artela.sh
+++ artela.sh
@@ -80,6 +80,7 @@ sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"20000000000uart\"|"
 # set peers and seeds
 SEEDS="211536ab1414b5b9a2a759694902ea619b29c8b1@47.251.14.47:26656,d89e10d917f6f7472125aa4c060c05afa78a9d65@47.251.32.165:26656,bec6934fcddbac139bdecce19f81510cb5e02949@47.254.24.106:26656,32d0e4aec8d8a8e33273337e1821f2fe2309539a@47.88.58.36:26656,1bf5b73f1771ea84f9974b9f0015186f1daa4266@47.251.14.47:26656"
 PEERS="db1d79226d9c735475bea5ba5a3c6a09f670d8d6@65.108.238.61:11656,5c4ea81ac7b8a7f5202fcbe5fe790a6d6f61fb22@47.251.14.108:26656,aa416d3628dcce6e87d4b92d1867c8eca36a70a7@47.254.93.86:26656,978dee673bd447147f61aa5a1bdaabdfb8f8b853@47.88.57.107:26656"
+sed -i 's/rpc_servers = ""/rpc_servers = "47.254.66.177:26657,47.254.93.86:26657,47.89.230.117:26657"/' $HOME/.artelad/config/config.toml
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.artelad/config/config.toml
 
 # disable indexing
